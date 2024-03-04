5286d4ec3273caf74d756bb82a13dc7b3511f4e0
Alejandro Zaizar <alexzaizar09@gmail.com>
2024-03-03 18:50:31 -0600

    passing argument to stimulus action

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class FavoriteToggleController extends Controller {
-  connect(): void {
-    console.log("Connected");
+  toggle({ params: {text}}): void {
+    console.log(text);
   }
 }
