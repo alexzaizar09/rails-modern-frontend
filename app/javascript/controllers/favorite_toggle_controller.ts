ed3220cfe40dc2f49db7780e9bac56c12148495e
Alejandro Zaizar <alexzaizar09@gmail.com>
2024-03-03 19:55:37 -0600

    adding stimulus values

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -2,11 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class FavoriteToggleController extends Controller {
-  static targets = ["elementToHide"]
+  static targets = ["elementToHide", "elementWithText"]
 
   elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
 
   toggle(): void {
-    this.elementToHideTarget.classList.toggle("hidden")
+    this.toggleVisibilityState()
+    this.toggleButtonText()
+    this.toggleElementVisibility()
+  }
+
+  toggleVisibilityState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  toggleButtonText(): void {
+    this.elementWithTextTarget.innerText = this.visibleValue ? "Hide" : "Show"
+  }
+
+  toggleElementVisibility(): void {
+    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
   }
 }
