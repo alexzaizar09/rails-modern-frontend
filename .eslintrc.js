8f3962f95446b3222dacff5964616070d167037c
Alejandro Zaizar <alexzaizar09@gmail.com>
2024-03-04 15:22:07 -0600

    adding react setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,4 @@
-/***
- * Excerpted from "Modern Front-End Development for Rails, Second Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit https://pragprog.com/titles/nrclient2 for more book information.
-***/
+
 module.exports = {
   env: {
     browser: true,
@@ -13,6 +6,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
