07b6e3fce4dc6d13f0959b855d670d426619154e
Alejandro Zaizar <alexzaizar09@gmail.com>
2024-03-05 12:30:49 -0600

    adding useState to Seat component

--- app/javascript/components/venue/row.tsx
+++ app/javascript/components/venue/row.tsx
@@ -8,7 +8,7 @@ interface RowProps {
 
 const Row = (props: RowProps): React.ReactElement => {
   const seats = Array.from(Array(props.seatsPerRow).keys()).map(
-    (seatNumber) => <Seat key={seatNumber} seatNumber={seatNumber} />
+    (seatNumber) => <Seat key={seatNumber} seatNumber={seatNumber} initialStatus="unsold" />
   )
 
   return <div className="flex items-center justify-center gap-4">{seats}</div>
--- app/javascript/components/venue/seat.tsx
+++ app/javascript/components/venue/seat.tsx
@@ -2,12 +2,33 @@ import * as React from "react"
 
 interface SeatProps {
   seatNumber: number
+  initialStatus: string
 }
 
-const Seat = (props: SeatProps): React.ReactElement => {
+const Seat = ({seatNumber, initialStatus}: SeatProps): React.ReactElement => {
+  const [status, setStatus] = React.useState(initialStatus)
+
+  function changeStatus(): void {
+    if (status === "held") {
+      setStatus("unsold")
+    } else {
+      setStatus("held")
+    }
+  }
+
+  function stateDisplayClass(): string {
+    if (status === "held") {
+      return "bg-green-500"
+    } else {
+      return "bg-white hover:bg-blue-300"
+    }
+  }
+
+  const cssClass = "flex items-center justify-center w-20 h-20 text-lg border-4 border-black button hover:bg-blue-300"
+
   return (
-    <span className="flex items-center justify-center w-20 h-20 text-lg border-4 border-black button hover:bg-blue-300">
-      {props.seatNumber + 1}
+    <span className={`${cssClass} ${stateDisplayClass()}`} onClick={changeStatus}>
+      {seatNumber + 1}
     </span>
   )
 }
